80245168d89947f04f40b34eccd8b8d62e1a0f6b
Hanne Kottum <HKOT@equinor.com>
2021-01-13 11:40:47 +0100

    eslint should now work

--- src/types/state.ts
+++ src/types/state.ts
@@ -19,5 +19,4 @@ export interface GlobalStateContext {
     state: Atom<GlobalState>;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export type EchoCustomState<T> = Partial<T>;
