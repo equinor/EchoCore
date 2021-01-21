05b2a07746635eb50f0bada540b1a83b596a9cc7
Christopher Berge Hove <chris@nogglings.com>
2021-01-21 13:58:38 +0100

    plant settings

---
+++ src/__tests__/settings/plantSettingActions.test.ts
@@ -0,0 +1,49 @@
+import { getSelectedPlant, setSelectedPlant } from '../../settings/plantSettingsActions';
+import { legendOptions, settings } from '../../state/defaultStates';
+import { dispatch } from '../../state/globalActions';
+import { getCoreContext } from '../../state/globalState';
+import { ActivePanel } from '../../types/panel';
+import { PlantSettings } from '../../types/settings';
+
+beforeEach(() => {
+    initialize();
+});
+
+const globalInit = {
+    modules: [],
+    panels: [],
+    activePanel: ActivePanel.None,
+    activeModule: '',
+    moduleState: {},
+    userProfile: undefined,
+    userPhotoUrl: undefined,
+    legendOptions,
+    settings
+};
+
+function initialize(): void {
+    dispatch(getCoreContext(), () => globalInit);
+}
+
+describe('plantSettingActions', () => {
+    const plantSettings: PlantSettings = {
+        selectedInstCode: 'JSV',
+        selectedSapPlantId: 'JSV',
+        selectedProCoSysPlantId: 'JSV',
+        selectedPlantName: 'Johann Sverdrup'
+    };
+
+    describe('setSelectedPlant', () => {
+        it('should update or set the PlantSettings', () => {
+            setSelectedPlant(plantSettings);
+        });
+    });
+
+    describe('getSelectedPlant', () => {
+        it('should get the PlantSettings', () => {
+            setSelectedPlant(plantSettings);
+            const result = getSelectedPlant();
+            expect(plantSettings).toEqual(result);
+        });
+    });
+});
---
+++ src/settings/plantSettingsActions.ts
@@ -0,0 +1,32 @@
+import { readState } from '../state/globalActions';
+import { getCoreContext } from '../state/globalState';
+import { PlantSettings } from '../types/settings';
+import { setSetting } from './globalSettingsActions';
+
+/**
+ * Used for setting/updating the plantSelected.
+ *
+ * @export Function from Echo Core.
+ * @param {PlantSettings} plantSettings selected plant data object.
+ */
+export function setSelectedPlant(plantSettings: PlantSettings): void {
+    setSetting(plantSettings);
+}
+
+/**
+ * Used fro retrieving the selected PlantData.
+ *
+ * @export Function from Echo Core.
+ * @return {*}  {PlantSettings}
+ */
+export function getSelectedPlant(): PlantSettings {
+    const { selectedInstCode, selectedSapPlantId, selectedProCoSysPlantId, selectedPlantName } = readState(
+        getCoreContext(),
+        (state) => {
+            return state.settings;
+        }
+    );
+
+    const selectedPlant = { selectedInstCode, selectedSapPlantId, selectedProCoSysPlantId, selectedPlantName };
+    return selectedPlant;
+}
