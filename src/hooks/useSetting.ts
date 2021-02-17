7b0c074c1355b399216416ae055a2aaf8b3b0a7a
Christopher Berge Hove <chris@nogglings.com>
2021-02-17 07:36:19 +0100

    useSettings Hook

--- src/hooks/useSetting.ts
+++ src/hooks/useSetting.ts
@@ -1,37 +1,61 @@
 import { useAtom } from '@dbeining/react-atom';
 import { useCallback } from 'react';
-import { getSelectedPlant, setSelectedPlant } from '../settings/plantSettingsActions';
+import { setSelectedPlant } from '../settings/plantSettingsActions';
 import { getCoreState } from '../state/globalState';
 import { Settings, UsePlantSettings } from '../types/settings';
 
 /**
- * Echo Core hook function for getting the settings form echo core state.
- *
- * @export Function fom Echo Core
+ * Internal Echo Core hook function for getting the settings form echo core state.
  * @return {*}  {Settings}
  */
-export default function useSettings(): Settings {
+function useSettings(): Settings {
     const { settings } = useAtom(getCoreState());
     return settings;
 }
 
 /**
  * Echo Core hook function for getting the plant settings form echo core state.
- * and functions gor get and set if needed.
- * @export Function fom Echo Core
+ * and function updating the plantSettings.
+ * @export Hook fom Echo Core
  * @return {*}  {UsePlantSettings}
  */
 export function usePlantSettings(): UsePlantSettings {
     const { instCode, sapPlantId, plantName, proCoSysPlantId } = useSettings();
-
-    const getPlantSettings = useCallback(getSelectedPlant, []);
     const setPlantSettings = useCallback(setSelectedPlant, []);
     return {
         instCode,
         sapPlantId,
         plantName,
         proCoSysPlantId,
-        getPlantSettings,
         setPlantSettings
     };
+}
+/**
+ * Echo Core hook function for getting the plant stid instCode.
+ * @export Hook form Echo Core
+ * @return {*}  instCode
+ */
+export function useInstCode(): string {
+    const { instCode } = useSettings();
+    return instCode;
+}
+
+/**
+ * Echo Core hook function for getting the plant sap PlantId.
+ * @export Hook form Echo Core
+ * @return {*}  sapPlantId
+ */
+export function useSapPlantIdInstCode(): string {
+    const { sapPlantId } = useSettings();
+    return sapPlantId;
+}
+
+/**
+ * Echo Core hook function for getting the plant proCoSysPlantId.
+ * @export Hook form Echo Core
+ * @return {*}  {string}
+ */
+export function useProCoSysPlantId(): string {
+    const { proCoSysPlantId } = useSettings();
+    return proCoSysPlantId;
 }
