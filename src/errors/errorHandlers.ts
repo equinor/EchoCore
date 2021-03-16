7d6edc8b4fe40b25297dd8a1e8316f9007961769
theVoogie <vugar.abdulzada@webstep.no>
2021-03-16 09:26:41 +0100

    Added generic error handler

--- src/errors/errorHandlers.ts
+++ src/errors/errorHandlers.ts
@@ -1,30 +1,35 @@
+import { NetworkError } from '..';
 import BaseError from './BaseError';
-import NetworkError from './NetworkError';
 import { CommonErrorArgs, ErrorInitializerFunction, NetworkErrorArgs } from './types';
+
+function factory<T extends BaseError, U extends CommonErrorArgs>(ErrorType: { new (args): T }, args: U): BaseError {
+    const errorInstance: T = new ErrorType(args);
+    return errorInstance;
+}
 
 const handleError = (errorInstance) => {
     console.log('handle >', errorInstance);
 };
 
-export const initializeError: ErrorInitializerFunction<NetworkError, NetworkErrorArgs> = (
+const initializeError: ErrorInitializerFunction<NetworkError, NetworkErrorArgs> = (
     ErrorType,
     args,
-    extendedInitializerFunction
+    extentionFunction
 ) => {
     let errorInstance: BaseError;
     if (args.exception?.errors) {
-        const { exception, httpStatusCode } = args;
-        switch (httpStatusCode) {
+        const { exception, statusCode } = args;
+        const isValidationError = exception.title && (exception.title as string).toLowerCase().includes('validation');
+        switch (statusCode) {
             case 400:
-                const message =
-                    exception.title && (exception.title as string).toLowerCase().includes('validation')
-                        ? 'ValidationError'
-                        : 'BackendError';
-                errorInstance = new ErrorType({ ...args, message });
-                break;
+                if (isValidationError) {
+                    errorInstance = new ErrorType({ ...args, message: 'ValidationError' });
+                    break;
+                }
             case 403:
                 errorInstance = new ErrorType({ ...args, message: 'ForbiddenError' });
                 break;
+
             case 404:
                 errorInstance = new ErrorType({ ...args, message: 'NotFoundError' });
                 break;
@@ -33,35 +38,26 @@ export const initializeError: ErrorInitializerFunction<NetworkError, NetworkErro
                 break;
         }
     } else {
-        errorInstance = extendedInitializerFunction ? extendedInitializerFunction() : new ErrorType(args);
+        errorInstance = extentionFunction ? extentionFunction() : new ErrorType(args);
     }
 
     return errorInstance;
 };
 
-export const factory = <T extends BaseError, U extends CommonErrorArgs>(
-    ErrorType: { new (args): T },
-    args: U
-): BaseError => {
-    const errorInstance: T = new ErrorType(args);
-    return errorInstance;
-};
-
-export const handleClientError = (
+export function handleClientError(
     exception: Record<string, unknown>,
     statusCode: number,
     endpoint: string,
-    extendedInitializerFunction?: () => BaseError,
     customHandler?: (errorInstance: BaseError) => void
-): BaseError => {
+): BaseError {
     const args: NetworkErrorArgs = {
         message: '',
         httpStatusCode: statusCode,
         url: endpoint,
         exception
     };
-    const errorInstance = initializeError(NetworkError, args, extendedInitializerFunction);
-    customHandler ? customHandler(errorInstance) : handleError(errorInstance);
+    const errorInstance = initializeError(NetworkError, args);
+    customHandler?.(errorInstance) || handleError(errorInstance);
 
     return errorInstance;
-};
+}
