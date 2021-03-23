827ce45d3795acaeecafd9f669abc005cf72d710
Christopher Berge Hove <chris@nogglings.com>
2021-03-24 00:45:59 +0100

    Add and remove from statae with key

---
+++ src/utils/state.ts
@@ -0,0 +1,11 @@
+export function addOrOverwriteWithKey<T, TKey extends keyof T>(obj: T, key: TKey, value: T[TKey]): T {
+    return {
+        ...obj,
+        [key]: value
+    };
+}
+
+export function removeWithKey<T, TKey extends keyof T>(obj: T, key: TKey): T {
+    const { [key]: _, ...newObj } = obj;
+    return newObj as T;
+}
