b807666d17fd2ccee04454dabc72a47370e0cba9
Christopher Berge Hove <chris@nogglings.com>
2021-03-27 23:09:43 +0100

    globael state

--- src/state/useGlobalState.ts
+++ src/state/useGlobalState.ts
@@ -3,8 +3,19 @@ import { GlobalState } from '../types/state';
 import { getCoreState } from './globalState';
 
 /**
- * Hook for handling the application module state object.
+ * Hook that yields the full global state.
+ * Any change to the global state yields the new state.
  */
-export default function useGlobalState(): GlobalState {
-    return useAtom(getCoreState());
+export function useGlobalState(): GlobalState;
+
+/**
+ * Hook that yields the selected subset of the global state.
+ * Only changes to this subset will yield a new state.
+ * @param select The subset selection.
+ */
+export function useGlobalState<R>(select: (state: GlobalState) => R): R;
+
+export function useGlobalState<R>(select?: (state: GlobalState) => R): GlobalState | R {
+    const state = useAtom(getCoreState());
+    return typeof select === 'function' ? select(state) : state;
 }
