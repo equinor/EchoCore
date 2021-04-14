fe1cbc541e89f6761a46e9ae3dc06bfe38deb616
jorgenholme96 <jorgen.holme@bouvet.no>
2021-04-14 11:22:15 +0200

    Rename src to packages

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    coveragePathIgnorePatterns: ['src/types/*', 'src/index.ts', 'src/settings/index.ts'],
-    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
+    coveragePathIgnorePatterns: ['packages/types/*', 'packages/index.ts', 'packages/settings/index.ts'],
+    collectCoverageFrom: ['packages/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
     coverageThreshold: {
         global: {
             lines: 90,
