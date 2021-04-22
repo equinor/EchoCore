59eb94af80abbb0f7c917803a177a04169351c5f
Christopher Berge Hove <chris@nogglings.com>
2021-04-22 11:31:10 +0200

    cleanup and integrity

--- packages/echo-base/src/module/dependency.tsx
+++ packages/echo-base/src/module/dependency.tsx
@@ -17,11 +17,11 @@ function requireModule(name: string, dependencies: AvailableDependencies): any {
     return dependency;
 }
 
-function checkApp(app?: AppData): AppData {
+function checkApp(name: string, app?: AppData): AppData {
     if (!app) {
-        console.error('Invalid module found.', app);
+        console.error('Invalid module found.', name);
     } else if (typeof app.setup !== 'function') {
-        console.warn('Setup function is missing.');
+        console.warn('Setup function is missing.', name);
     } else {
         return app;
     }
@@ -37,26 +37,34 @@ export async function includeScript(
     link: string,
     depName: string,
     dependencies: AvailableDependencies,
-    crossOrigin?: string
+    crossOrigin?: string,
+    integrity?: string
 ): Promise<AppData | undefined> {
     return new Promise<AppData | undefined>((resolve, reject) => {
         const script = document.createElement('script');
         script.async = true;
         script.src = link;
-        script.crossOrigin = crossOrigin ? crossOrigin : '';
+        script.id = name;
+
+        if (integrity) {
+            script.crossOrigin = crossOrigin || 'anonymous';
+            script.integrity = integrity;
+        } else if (crossOrigin) {
+            script.crossOrigin = crossOrigin;
+        }
 
         window[depName] = getLocalRequire(dependencies);
 
-        script.onload = (): void => resolve(checkAppAsync(script.app));
+        script.onload = (): void => resolve(checkAppAsync(name, script.app));
         script.onerror = (): void => reject('could not load');
 
         document.head.appendChild(script);
     });
 }
 
-export async function checkAppAsync(app?: AppData | Promise<AppData>): Promise<AppData> {
+export async function checkAppAsync(name: string, app?: AppData | Promise<AppData>): Promise<AppData> {
     const resolvedApp = await Promise.resolve(app);
-    return checkApp(resolvedApp);
+    return checkApp(name, resolvedApp);
 }
 
 export function getLocalRequire(dependencies: AvailableDependencies = {}) {
@@ -64,17 +72,17 @@ export function getLocalRequire(dependencies: AvailableDependencies = {}) {
 }
 
 export async function includeDependency(
-    { name, link, requireRef }: SingleAppMetadata,
+    { name, link, requireRef, integrity }: SingleAppMetadata,
     dependencies?: AvailableDependencies,
     crossOrigin?: string
 ): Promise<AppData> {
-    return await includeScript(name, link, requireRef, dependencies, crossOrigin);
+    return await includeScript(name, link, requireRef, dependencies, crossOrigin, integrity);
 }
 
 export function includeBundle(
-    { name, link, integrity }: MultiAppsMetadata,
+    { name, link, bundle, integrity }: MultiAppsMetadata,
     dependencies?: AvailableDependencies,
     crossOrigin?: string
 ): Promise<AppData> {
-    return includeScript(name ?? '(bundle)', link, integrity, dependencies, crossOrigin);
+    return includeScript(name ?? '(bundle)', link, bundle, dependencies, crossOrigin, integrity);
 }
