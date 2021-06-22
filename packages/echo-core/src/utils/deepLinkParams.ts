7a7ce2b09ff6005d067a8d6106bdb63b861a389f
Christopher Berge Hove <chris@nogglings.com>
2021-06-22 14:55:55 +0200

    doc: added dpocumentation

--- packages/echo-core/src/utils/deepLinkParams.ts
+++ packages/echo-core/src/utils/deepLinkParams.ts
@@ -7,6 +7,22 @@ export enum DeepLinkParameters {
 export const availableSearchParams = ['instCode', 'tagNo', 'search'];
 
 type LinkParam = { [key: string]: string | null };
+
+/**
+ * This function wil extract the query parameter in the browser url
+ * if present a string value will be present and if not the value wil be null.
+ * The in param wil be a list if the desired parameters,
+ * default parameter are:
+ *
+ * ```
+ *  ['instCode', 'tagNo', 'search'];
+ * ```
+ *
+ * @export
+ * @template T
+ * @param {Array<keyof LinkParam>} [params=availableSearchParams]
+ * @return {*}  {Required<T>} Will return a object with the provided parameters, but the value may be strung or null
+ */
 export function getLinkParams<T extends LinkParam>(
     params: Array<keyof LinkParam> = availableSearchParams
 ): Required<T> {
@@ -17,7 +33,12 @@ export function getLinkParams<T extends LinkParam>(
     });
     return linkParams as Required<T>;
 }
-
+/**
+ * this function will replace the query parameters on det current url
+ *
+ * @param {string} param
+ * @param {string} value
+ */
 export const setDeepLinkParams = (param: string, value: string): void => {
     const queryParams = new URLSearchParams(new URL(window.location.href).search);
     if (value.length > 0) {
