99822006068a75132088b777a8fe260d43395b0e
Csaba Kiss <ckiss@equinor.com>
2021-09-21 14:17:50 +0200

    Version bump + added function descriptions

--- packages/echo-core/src/services/api/api-plantInfo.ts
+++ packages/echo-core/src/services/api/api-plantInfo.ts
@@ -11,6 +11,10 @@ export interface EchoHubPlant {
     ayelixSiteId?: number;
 }
 
+/**
+ * Fetches plant information from API
+ * @param {string} instCode Code of the plant
+ */
 export async function getPlantInfoFromApi(instCode?: string): Promise<EchoHubPlant[]> {
     let url = `${baseApiUrl}/EchoHub/plant-info`;
     if (instCode) {
--- packages/echo-core/src/services/api/api-tags.ts
+++ packages/echo-core/src/services/api/api-tags.ts
@@ -1,6 +1,13 @@
 import { RequestTagDetails, TagDetails } from '../../types/tagDetails';
 import { baseApiUrl, request } from './api-manager';
 
+/**
+ * Fetches tag details from API for a given tag
+ * @param {Object} {
+ *      instCode: {string} Code for the plant where the tag is located
+ *      tagNo: {string} Tag number
+ * }
+ */
 export async function getTagDetails({ instCode, tagNo }: RequestTagDetails): Promise<TagDetails> {
     const url = `${baseApiUrl}/${instCode}/tag?tagNo=${tagNo}`;
     return await request<TagDetails>(url, {} as TagDetails);
