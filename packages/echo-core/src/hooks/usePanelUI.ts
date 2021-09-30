2b86b5d2e49d15f9b20540727fa392557951dd85
Jørgen Holme <74668394+jorgenholme96@users.noreply.github.com>
2021-09-30 14:21:35 +0200

    Add panel UI (#71)

---
+++ packages/echo-core/src/hooks/usePanelUI.ts
@@ -0,0 +1,12 @@
+import { useAtom } from '@dbeining/react-atom';
+import { CoreContext } from '../state/globalState';
+import { PanelUI } from '../types/ui';
+
+/**
+ *Echo Core hook for handling non-default panel ui.
+ * @return {PanelUI}  Reads the ui state and returns a partial ui-object of type PanelUI
+ */
+export function usePanelUI(): PanelUI {
+    const { ui } = useAtom(CoreContext.state);
+    return ui;
+}
