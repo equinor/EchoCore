393433cdc274401364c5275704146b69528728a2
Ove Byberg <57181933+OveBDev@users.noreply.github.com>
2021-10-22 08:24:10 +0200

    feat: try Catch unknown => toError (#78)

---
+++ packages/echo-base/src/errors/toError.test.ts
@@ -0,0 +1,136 @@
+import { BaseError } from './BaseError';
+import { ForbiddenError } from './NetworkError';
+import { ImproperErrorObject, toError } from './toError';
+
+describe('ConvertToErrorIfNeeded', () => {
+    it('should return error if error', () => {
+        const error = new Error('error');
+        const actual = toError(error);
+        expect(actual).toBe(error);
+    });
+
+    it('should return baseError if baseError', () => {
+        const error = new BaseError({ message: 'error' });
+        const actual = toError(error);
+        expect(actual).toBe(error);
+    });
+
+    it('should return correct subType of baseError, eg should NotImplementedError if NotImplementedError', () => {
+        const error = new ForbiddenError({ httpStatusCode: 403, url: 'https://fakeurl' });
+        const actual = toError(error);
+        expect(actual).toBe(error);
+    });
+
+    it(`should convert 'string' to NotAProperErrorObject`, () => {
+        try {
+            throw 'this is not a proper error';
+        } catch (error) {
+            // when
+            const actual = toError(error);
+
+            // then
+            const expected = new ImproperErrorObject({
+                message: 'this is not a proper error',
+                exception: { errorArgumentType: 'string' }
+            });
+
+            expect(actual).toStrictEqual(expected);
+        }
+    });
+
+    it(`should convert '42' to NotAProperErrorObject`, () => {
+        try {
+            throw 42;
+        } catch (error) {
+            // when
+            const actual = toError(error);
+
+            // then
+            const expected = new ImproperErrorObject({
+                message: '42',
+                exception: { errorArgumentType: 'number' }
+            });
+
+            expect(actual).toStrictEqual(expected);
+        }
+    });
+
+    it(`should convert 'false' to NotAProperErrorObject`, () => {
+        try {
+            throw false;
+        } catch (error) {
+            // when
+            const actual = toError(error);
+
+            // then
+            const expected = new ImproperErrorObject({
+                message: 'false',
+                exception: { errorArgumentType: 'boolean' }
+            });
+
+            expect(actual).toStrictEqual(expected);
+        }
+    });
+
+    it(`should convert 'symbol' to NotAProperErrorObject`, () => {
+        try {
+            throw Symbol('key');
+        } catch (error) {
+            // when
+            const actual = toError(error);
+
+            // then
+            const expected = new ImproperErrorObject({
+                message: 'Symbol(key)',
+                exception: { errorArgumentType: 'symbol' }
+            });
+
+            expect(actual).toStrictEqual(expected);
+        }
+    });
+
+    it(`should convert 'object' to NotAProperErrorObject, and keep all properties`, () => {
+        try {
+            throw { prop1: 'a', name: 'name', message: 'message' };
+        } catch (error) {
+            // when
+            const actual = toError(error);
+
+            // then
+            const expected = new ImproperErrorObject({
+                message: 'name message',
+                exception: { prop1: 'a', name: 'name', message: 'message a', errorArgumentType: 'object' }
+            });
+
+            expect(actual).toStrictEqual(expected);
+        }
+    });
+
+    it(`should convert 'class not extending error' to NotAProperErrorObject, and keep all properties`, () => {
+        try {
+            throw new CustomErrorNotExtendingError();
+        } catch (error) {
+            // when
+            const actual = toError(error);
+
+            // then
+            const expected = new ImproperErrorObject({
+                message: 'name message a',
+                exception: { prop1: 'a', name: 'name', message: 'message a', errorArgumentType: 'object' }
+            });
+
+            expect(actual).toStrictEqual(expected);
+        }
+    });
+});
+
+class CustomErrorNotExtendingError {
+    private prop1: string;
+    name: string;
+    message: string;
+    constructor() {
+        this.prop1 = 'a';
+        this.name = 'name';
+        this.message = 'message ' + this.prop1;
+    }
+}
---
+++ packages/echo-base/src/errors/toError.ts
@@ -0,0 +1,66 @@
+import { BaseError } from './BaseError';
+
+export class ImproperErrorObject extends BaseError {}
+
+/**
+ * Helper method: Useful with Try Catch(error), where error is of type unknown.
+ * Returns same error if it's already an error (or if it extends error),
+ * otherwise it converts it to type NotAProperErrorObject (with all properties preserved) which extends baseError
+ * @param error The error to convert (if needed)
+ * @returns Error or a an object which extends Error.
+ */
+export function toError(error: Error | BaseError | unknown): Error | BaseError {
+    if (error instanceof BaseError || error instanceof Error) {
+        return error;
+    }
+
+    const errorType = typeof error;
+    if (
+        errorType === 'string' ||
+        errorType === 'number' ||
+        errorType === 'boolean' ||
+        errorType === 'bigint' ||
+        errorType === 'symbol'
+    ) {
+        let message = 'unknown';
+        switch (errorType) {
+            case 'string':
+                message = error as string;
+                break;
+            case 'number':
+                message = (error as number).toString();
+                break;
+            case 'boolean':
+                message = (error as boolean).toString();
+                break;
+            case 'bigint':
+                message = (error as bigint).toString();
+                break;
+            case 'symbol':
+                message = (error as symbol).toString();
+                break;
+            default:
+                break;
+        }
+        return new ImproperErrorObject({
+            message: message,
+            exception: { argumentType: errorType }
+        });
+    }
+
+    if (errorType === 'object') {
+        const errorAsObject = error as object;
+        const properties = { ...errorAsObject } as { [key: string]: unknown };
+
+        const maybeName = (properties['name'] as string) ?? '';
+        const maybeMessage = (properties['message'] as string) ?? '';
+        const message = `${maybeName} ${maybeMessage}`.trim();
+
+        return new ImproperErrorObject({
+            message: message.length > 0 ? message : 'unknown',
+            exception: { ...properties, argumentType: errorType }
+        });
+    }
+
+    return new ImproperErrorObject({ message: 'unknown', exception: { argumentType: errorType } });
+}
