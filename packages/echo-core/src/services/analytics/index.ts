cec2a7b7f1f01492bb6eb927da9175fa6c243fac
Ove Byberg <57181933+OveBDev@users.noreply.github.com>
2021-11-03 08:20:33 +0100

    feat: 51610 analytics exclude filter: add telemetryItem (#81)

---
+++ packages/echo-core/src/services/analytics/index.ts
@@ -0,0 +1,9 @@
+export type { ITelemetryItem } from '@microsoft/applicationinsights-web';
+export { analytics, analyticsConfiguration } from './analytics';
+export { AnalyticsModule } from './analyticsModule';
+export type {
+    AnalyticsEvent,
+    AnalyticsEventName,
+    AnalyticsNameFilterFunction,
+    AnalyticsPropertyTypes
+} from './analyticsTypes';
