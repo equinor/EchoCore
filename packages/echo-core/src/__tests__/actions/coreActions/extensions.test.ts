11c4aadcdae99e189f42e243bc45d395f43a4b76
Csaba Kiss <ckiss@equinor.com>
2022-02-18 13:26:22 +0100

    Unit tests

--- packages/echo-core/src/__tests__/actions/coreActions/extensions.test.ts
+++ packages/echo-core/src/__tests__/actions/coreActions/extensions.test.ts
@@ -1,36 +1,123 @@
-import { registerExtension } from '../../../actions/coreActions/extensions';
+import { registerExtension, registerMultipleExtensions } from '../../../actions/coreActions/extensions';
 import { readState } from '../../../actions/coreActions/globalActions';
 import { getCoreContext } from '../../../state/globalState';
 import { ExtensionRegistration } from './../../../types/registry/extension.types';
 
-describe('registerExtension()', () => {
-    const mockComponent = () => {
-        return null;
-    };
+describe('extensions', () => {
+    describe('registerExtension()', () => {
+        it('should register and unregister a given extension into the state', () => {
+            // when
+            const unregisterExtension = registerExtension(extension);
+            let registeredExtensions = getExtensions();
 
-    const extension: ExtensionRegistration = {
-        key: 'unique-key',
-        extends: 'MyFavEchopediaWebComponent',
-        component: mockComponent
-    };
-    it('should register and unregister a given extension into the state', () => {
-        // given
+            // then
+            expect(registeredExtensions).toEqual({
+                MyFavEchopediaWebComponent: [
+                    { component: mockComponent, extends: 'MyFavEchopediaWebComponent', key: 'unique-key' }
+                ]
+            });
 
-        // when
-        const unregisterExtension = registerExtension(extension);
-        let registeredExtensions = readState(getCoreContext(), (state) => state.registry.extensions);
-
-        // then
-        expect(registeredExtensions).toEqual({
-            MyFavEchopediaWebComponent: [
-                { component: mockComponent, extends: 'MyFavEchopediaWebComponent', key: 'unique-key' }
-            ]
+            unregisterExtension();
+            registeredExtensions = getExtensions();
+            expect(registeredExtensions).toStrictEqual({
+                MyFavEchopediaWebComponent: []
+            });
         });
+    });
 
-        unregisterExtension();
-        registeredExtensions = readState(getCoreContext(), (state) => state.registry.extensions);
-        expect(registeredExtensions).toStrictEqual({
-            MyFavEchopediaWebComponent: []
+    describe('registerMultipleExtensions()', () => {
+        it('should register and unregister multiple extensions', () => {
+            // given
+            const extensionsToRegister: ExtensionRegistration[] = [
+                {
+                    ...extension
+                },
+                {
+                    ...extension,
+                    key: 'key-1'
+                },
+                {
+                    ...extension,
+                    key: 'key-2'
+                },
+                {
+                    ...extension,
+                    key: 'key-0',
+                    extends: 'TheBestEchopediaWebComponent'
+                }
+            ];
+
+            // when
+            const unregisterFunctions = registerMultipleExtensions(extensionsToRegister);
+
+            // then
+            expect(getExtensions()).toStrictEqual({
+                MyFavEchopediaWebComponent: [
+                    {
+                        key: 'unique-key',
+                        extends: 'MyFavEchopediaWebComponent',
+                        component: mockComponent
+                    },
+                    {
+                        key: 'key-1',
+                        extends: 'MyFavEchopediaWebComponent',
+                        component: mockComponent
+                    },
+                    {
+                        key: 'key-2',
+                        extends: 'MyFavEchopediaWebComponent',
+                        component: mockComponent
+                    }
+                ],
+                TheBestEchopediaWebComponent: [
+                    {
+                        key: 'key-0',
+                        extends: 'TheBestEchopediaWebComponent',
+                        component: mockComponent
+                    }
+                ]
+            });
+
+            unregisterFunctions[0]();
+            unregisterFunctions[2]();
+
+            expect(getExtensions()).toStrictEqual({
+                MyFavEchopediaWebComponent: [
+                    {
+                        key: 'key-1',
+                        extends: 'MyFavEchopediaWebComponent',
+                        component: mockComponent
+                    }
+                ],
+                TheBestEchopediaWebComponent: [
+                    {
+                        key: 'key-0',
+                        extends: 'TheBestEchopediaWebComponent',
+                        component: mockComponent
+                    }
+                ]
+            });
+
+            unregisterFunctions[1]();
+            unregisterFunctions[3]();
+            expect(getExtensions()).toStrictEqual({
+                MyFavEchopediaWebComponent: [],
+                TheBestEchopediaWebComponent: []
+            });
         });
     });
 });
+
+const mockComponent = () => {
+    return null;
+};
+
+const extension: ExtensionRegistration = {
+    key: 'unique-key',
+    extends: 'MyFavEchopediaWebComponent',
+    component: mockComponent
+};
+
+function getExtensions() {
+    return readState(getCoreContext(), (state) => state.registry.extensions);
+}
