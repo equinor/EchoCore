bd4478bd43661158154e61d02235610824cb8513
Ove Byberg <ove.byberg@bouvet.no>
2022-06-13 09:58:56 +0200

    cleanup

--- packages/echo-core/src/hooks/useUserProfileBeta.ts
+++ packages/echo-core/src/hooks/useUserProfileBeta.ts
@@ -1,11 +1,11 @@
 import { useAtom } from '@dbeining/react-atom';
-import { UserBeta } from '../services/graph/graphTypes';
+import { UserProfileBeta } from '../services/graph/graphTypes';
 import { CoreContext } from '../state/globalState';
 
 /**
  * Echo Core function for getting the graph user profile beta from echo core state.
  */
-export function useUserProfileBeta(): UserBeta | undefined {
+export function useUserProfileBeta(): UserProfileBeta | undefined {
     const { userProfileBeta } = useAtom(CoreContext.state);
     return userProfileBeta;
 }
