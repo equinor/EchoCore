37a701ef359bddfd2f476ae8dd523c8e8940a03b
Ove Byberg <57181933+OveBDev@users.noreply.github.com>
2022-07-04 10:33:38 +0200

    fix: 69299 Release v0.6.10, Export errorUtils & updated changelog (#136)
    * fix 69299: Release v0.6.10, Export errorUtils & updated changelog
    
    * echo-core use base 0.6.10

--- packages/echo-base/src/errors/index.ts
+++ packages/echo-base/src/errors/index.ts
@@ -1,5 +1,6 @@
 export * from './ArgumentError';
 export * from './BaseError';
 export * from './errorHandlers';
+export { errorUtils } from './errorUtils';
 export * from './NetworkError';
 export * from './toError';
